56c7390c9b9f6014783216710dc3f4412e103a0a
abhishekmalakar <malakar.abh@gmail.com>
2022-12-31 22:21:50 +0530

    Changes Made

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int a[]=new int[5];
+        for(int i=5;i>0;i--){
+            System.out.println();
+            for(int j=5;j>=i;j--){
+            System.out.print(i);
+        }}
+
+    }
+}
