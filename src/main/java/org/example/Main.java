6bfea7a1dc820dfe8294653ffc456daf5124fa07
abhishekmalakar <malakar.abh@gmail.com>
2022-12-31 22:26:16 +0530

    Changes Made

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        int a[]=new int[5];
+        //int a[]=new int[5];
         for(int i=5;i>0;i--){
             System.out.println();
             for(int j=5;j>=i;j--){
